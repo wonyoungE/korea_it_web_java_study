1bd5a89dec0c6bf2c6e7189425902d831d90c272
wonyoungE <error071312@gmail.com>
2025-05-13 21:46:04 +0900

    init: first commit

--- src/day01/Main.java
+++ src/day01/Main.java
@@ -3,6 +3,7 @@ package day01;
 public class Main { // 궁극적으로 자바를 실행시키는 코드
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("world");
 
     }
 }
