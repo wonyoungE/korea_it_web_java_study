6fa55f6991a2bef83e6aa28e36b7e86a4f049a7b
wonyoungE <error071312@gmail.com>
2025-05-14 19:12:37 +0900

    feat: 내용 추가

--- src/day01/Main.java
+++ src/day01/Main.java
@@ -4,6 +4,6 @@ public class Main { // 궁극적으로 자바를 실행시키는 코드
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("world");
-
+        System.out.println("!!");
     }
 }
