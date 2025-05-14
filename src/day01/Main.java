8dc434ab7445d2c6ed27e970d540bb8d0b6e8793
wonyoungE <error071312@gmail.com>
2025-05-14 19:19:35 +0900

    feat: 깃 실행 순서

--- src/day01/Main.java
+++ src/day01/Main.java
@@ -5,5 +5,11 @@ public class Main { // 궁극적으로 자바를 실행시키는 코드
         System.out.println("hello");
         System.out.println("world");
         System.out.println("!!");
+
+        // 1. git init -> 초기화
+        // 2. git status -> 현재 상태(내용 바뀐 파일 출력)
+        // 3. git add . -> 모두 다 올리기
+        // 4. git commit -m "" -> 커밋 메세지
+        // 5. git push origin "브랜치명" -> "브랜치명" 브랜치에 push
     }
 }
