cbf65ec2707247fe0e15bf84245b95cd9995e738
wonyoungE <error071312@gmail.com>
2025-05-20 21:49:56 +0900

    Feat: MultiArrayLoop

---
+++ src/_15_MultiArray/MultiArrayLoop.java
@@ -0,0 +1,56 @@
+package _15_MultiArray;
+
+public class MultiArrayLoop {
+    public static void main(String[] args) {
+        // 다차원 배열 순회
+        String[][] seats = new String[][] {
+                {"A1", "A2", "A3", "A4", "A5"},
+                {"B1", "B2", "B3", "B4", "B5"},
+                {"C1", "C2", "C3", "C4", "C5"}
+        };
+
+        // seats[0] => {"A1", "A2", "A3", "A4", "A5"}
+        // seats[0].length => 5
+        // seats[1] => {"B1", "B2", "B3", "B4", "B5"}
+        // seats[1].length => 5
+
+//        for (int i = 0; i < seats.length; i++) {
+//            for (int j = 0; j < seats[0].length; j++) {
+//                System.out.print(seats[i][j] + " ");
+//            }
+//            System.out.println();
+//        }
+
+        // 향상된 for문
+        for(String[] seatsArray : seats) {
+            for(String seat : seatsArray) {
+                System.out.print(seat + " ");
+            }
+            System.out.println();
+        }
+
+        String[][] imaxSeats = new String[10][15];
+        String[] eng = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J"};
+
+        for (int i = 0; i < imaxSeats.length; i++) {
+            for (int j = 0; j < imaxSeats[0].length; j++) {
+                imaxSeats[i][j] = eng[i] + (j + 1);
+            }
+        }
+
+        for(String[] seatsArray : imaxSeats) {
+            for(String seat : seatsArray) {
+                System.out.print(seat + " ");
+            }
+            System.out.println();
+        }
+
+        // 다차원 배열 합 구하기
+        // 3x3크기 1부터 홀수만 차례대로 대입하고 출력해서 확인
+        // 그 수들의 합 구하기
+        // 1 3 5
+        // 7 9 11
+        // 13 15 17
+
+    }
+}
