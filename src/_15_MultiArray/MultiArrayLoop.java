f2680be19ceeb66c7771098425b783379775e513
wonyoungE <error071312@gmail.com>
2025-05-21 19:15:41 +0900

    Feat: MultiArrayLoop 문제풀이

--- src/_15_MultiArray/MultiArrayLoop.java
+++ src/_15_MultiArray/MultiArrayLoop.java
@@ -45,6 +45,7 @@ public class MultiArrayLoop {
             System.out.println();
         }
 
+        // 숙제
         // 다차원 배열 합 구하기
         // 3x3크기 1부터 홀수만 차례대로 대입하고 출력해서 확인
         // 그 수들의 합 구하기
@@ -52,5 +53,30 @@ public class MultiArrayLoop {
         // 7 9 11
         // 13 15 17
 
+        // 1) 3x3 크기 2차원 배열 생성
+        int[][] oddMultiArray = new int[3][3];
+        // 2) 홀수만 차례대로 대입
+        for (int i = 0; i < oddMultiArray.length; i++) {
+            for (int j = 0; j < oddMultiArray[0].length; j++) {
+                oddMultiArray[i][j] = (6 * i) + (2 * j) + 1;
+            }
+        }
+
+        // 3) 대입한 값 확인
+        for(int[] row : oddMultiArray) {
+            for(int column : row) {
+                System.out.print(column + " ");
+            }
+            System.out.println();
+        }
+
+        // 4) 수들의 합 구하기
+        int sum = 0;
+        for(int[] row : oddMultiArray) {
+            for(int column : row) {
+                sum += column;
+            }
+        }
+        System.out.println("수들의 합: " + sum);
     }
 }
